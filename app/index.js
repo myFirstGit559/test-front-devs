a9f27ae3082850052ee4585cb0f1f730eb7d7841
tanya.agisheva@draytus.com <mp2258916>
2017-03-29 18:39:46 +0300

    set main modules of application

--- app/index.js
+++ app/index.js
@@ -1,5 +1,13 @@
 const styles = require('./less\/');
+//products data
 const men = require('./data/men.json');
 const women = require('./data/women.json');
 const children = require('./data/children.json');
+//images
 const logo = require('./img/logo_google.png');
+//angular app
+const angular = require('angular');
+const uirouter = require('angular-ui-router');
+const sanitize = require('angular-sanitize');
+const resource = require('angular-resource');
+const directiveModule = angular.module('shopApp', []);
