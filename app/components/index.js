10d37cad5f82b119e2aafb0d6680cc464e0866d7
Таня <agishevatanyaa@gmail.com>
2017-04-08 22:17:44 +0300

    add pagination filter count pages depends of screen width

--- app/components/index.js
+++ app/components/index.js
@@ -2,6 +2,7 @@ module.exports = function(shopApp){
     require('./catalog/catalog.service.js')(shopApp);
     require('./filter-menu/filter-menu.js')(shopApp);
     require('./select/select.js')(shopApp);
+    require('./slider/pagination.js')(shopApp);
     require('./slider/slider.js')(shopApp);
     require('./catalog/catalog.js')(shopApp);
     require('./inner/inner.js')(shopApp);
